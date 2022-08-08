1ce4afca8c62e5d29fd4225540b8ebd26671782e
infraboy <ayushkumarghosh@gmail.com>
2022-08-08 08:34:22 +0530

    done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Node Server
-// var http = require('http');
-// http.createServer((req,res)=>{
-//     res.write("Welcome to HTTP node Server");
-//     res.end();
-// }).listen(1234,()=>console.log("HTTP Server started at port 1234"));
+var http = require('http');
+http.createServer((req,res)=>{
+    res.write("Welcome to HTTP node Server");
+    res.end();
+}).listen(process.env.PORT || 1234,()=>console.log("HTTP Server started at port 1234"));
 
 //Express Server
 const express =require('express');
